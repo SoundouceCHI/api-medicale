0b2f91a1a43e32c2798e3483afaa09b0116e5266
unknown <soundouce.chibani@gmail.com>
2025-04-28 15:44:39 +0200

    Initial commit

---
+++ src/main/java/com/example/api_medicale/ApiMedicaleApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_medicale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMedicaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMedicaleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/api_medicale/ApiMedicaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api_medicale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMedicaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
